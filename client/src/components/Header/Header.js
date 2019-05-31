620f7237ece7351ef7efa2640930505232b0a7c6
124c4a11 <124c4a11@mail.ru>
2019-05-31 13:05:13 +0300

    CLIENT: Header nav list layout

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,126 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import Button from '../Button';
 
 import './Header.scss';
 
 
-export default class Header extends Component {
+class Header extends Component {
+  state = {
+    pages: [
+      {
+        name: 'Home',
+        linkTo: '/',
+        public: true
+      },
+      {
+        name: 'Guitars',
+        linkTo: '/shop',
+        public: true
+      }
+    ],
+
+    user: [
+      {
+        name: 'My Cart',
+        linkTo: '/user/cart',
+        public: false
+      },
+      {
+        name: 'My Account',
+        linkTo: '/user/dashboard',
+        public: false
+      },
+      {
+        name: 'Log in',
+        linkTo: '/login',
+        public: true
+      },
+      {
+        name: 'Log out',
+        linkTo: '/user/logout',
+        public: false
+      }
+    ]
+  };
+
+  generateMenuItems = (links) => {
+    const user = this.props.user.userData;
+
+    return links.map((link) => {
+      switch (link.name) {
+        case 'My Cart':
+          return (
+            <li
+              className="header__nav-list-item"
+              key={ link.name }
+            >
+              <Link
+                className="header__nav-list-link"
+                to={ link.linkTo }
+              >
+                <span className="header__nav-list-badge">
+                  { user.cart ? user.cart.length : 0 }
+                </span>
+                { link.name }
+              </Link>
+            </li>
+          );
+
+        case 'Log out':
+          return (
+            <li
+              className="header__nav-list-item"
+              key={ link.name }
+            >
+              <Button
+                title={ link.name }
+                onClick={ (e) => this.onLogout(e) }
+                className="header__nav-list-link"
+              />
+            </li>
+          );
+
+        default:
+          return (
+            <li
+              className="header__nav-list-item"
+              key={ link.name }
+            >
+              <Link
+                className="header__nav-list-link"
+                to={ link.linkTo }
+              >{ link.name }</Link>
+            </li>
+          );
+      }
+    });
+  };
+
+  generateMenuList = (links) => {
+    const user = this.props.user.userData;
+
+    let listLinks = [];
+
+    if (user) {
+      links.forEach((link) => {
+        if (!user.isAuth) {
+          if (link.public) listLinks.push(link);
+        } else {
+          if (link.name !== 'Log in') listLinks.push(link);
+        }
+      });
+    }
+
+    return (
+      <ul className="header__nav-list">
+        { this.generateMenuItems(listLinks) }
+      </ul>
+    );
+  }
+
   render() {
     return (
       <header className="header">
@@ -11,16 +128,21 @@ export default class Header extends Component {
           <div className="header__logo">
             <a href="/" className="logo">WAVES</a>
           </div>
-          <div className="header__nav">
+          <nav className="header__nav">
             <div className="header__nav-top">
-              LINKS
+              { this.generateMenuList(this.state.user) }
             </div>
             <div className="header__nav-bottom">
-              LINKS
+              { this.generateMenuList(this.state.pages) }
             </div>
-          </div>
+          </nav>
         </div>
       </header>
     );
   }
 };
+
+
+export default connect(({ user }) => ({
+  user
+}))(Header);
