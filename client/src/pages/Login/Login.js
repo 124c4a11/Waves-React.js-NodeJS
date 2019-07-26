ee49a4027f230cdf7a6ab2d42828b2177cf6be95
124c4a11 <124c4a11@mail.ru>
2019-07-26 18:46:34 +0300

    CLIENT: Modal responsive

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -8,7 +8,7 @@ export default () => {
   return (
     <div className="container container_vertical-centerer">
       <div className="modal modal_flex">
-        <div className="modal__col">
+        <div className="modal__col modal__col_order-sm-1">
           <h2 className="modal__title mt-0">New Customers</h2>
           <p>Fugiat non ipsum adipisicing cillum et eiusmod Lorem pariatur in occaecat nisi. Duis non excepteur quis sint enim. Commodo sit dolor eu sit sit in ullamco. Commodo ullamco sint deserunt nisi ex sint nostrud sunt quis. Ea incididunt dolor nulla commodo nostrud magna qui enim quis commodo ipsum. Ut laborum magna dolor duis incididunt excepteur aliqua anim esse ea in.</p>
 
