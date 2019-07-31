0418c38c8e0fe7856276819fb57e7da34e4ed484
124c4a11 <124c4a11@mail.ru>
2019-07-31 16:54:29 +0300

    CLIENT: react router scroll top

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from 'react-router-scroll-top';
 import store from './store';
 
 import Routes from './routes';
@@ -12,7 +13,9 @@ import './styles/index.scss';
 ReactDOM.render(
   <Provider store={ store }>
     <Router>
-      <Routes />
+      <ScrollToTop>
+        <Routes />
+      </ScrollToTop>
     </Router>
   </Provider>
   , document.getElementById('root')
